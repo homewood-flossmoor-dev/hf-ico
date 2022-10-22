b8f6b60138e469f310dbcb8942a35a7bcdaa369b
Juan Romero <75763058+jromerooo2@users.noreply.github.com>
2022-10-22 17:59:32 -0500

    structure of the ICO

--- index.js
+++ index.js
@@ -1,2 +1,34 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+class Wallet {
+}
+class Block {
+    constructor(index, hash, previousHash, timestamp, data) {
+        this.index = index;
+        this.hash = hash;
+        this.previousHash = previousHash;
+        this.timestamp = timestamp;
+        this.data = data;
+    }
+    get_hash() {
+    }
+}
+class Transaction {
+    constructor(amount, payer, //public key
+    payee //public key
+    ) {
+        this.amount = amount;
+        this.payer = payer;
+        this.payee = payee;
+    }
+}
+class Chain {
+    constructor() {
+        this.chain = [
+            //GENESIS BLOCK (1ST BLOCK HARD CODED)
+            new Block(0, "asdadadadasd", "asadasdadas", new Date().getTime(), new Transaction(100, "genesis", "satoshi"))
+        ];
+    }
+}
+//singleton instance
+Chain.instance = new Chain();
--- index.ts
+++ index.ts
@@ -1,2 +1,46 @@
 import * as crypto from 'crypto';
 
+class Wallet {
+
+}
+
+class Block{
+    public index: number;
+    public hash: string;
+    public previousHash: string;
+    public timestamp: number;
+    public data: Transaction;
+
+    constructor(index:number, hash:string, previousHash:string, timestamp:number, data:Transaction){
+        this.index = index;
+        this.hash = hash;
+        this.previousHash = previousHash;
+        this.timestamp = timestamp;
+        this.data = data;
+    }
+
+    get_hash(){
+
+    }
+
+}
+class Transaction{
+    constructor(
+        public amount: number,
+        public payer: string, //public key
+        public payee: string //public key
+    ){}
+}
+
+class Chain{
+    //singleton instance
+    public static instance = new Chain();
+    chain: Block[];
+
+    constructor(){
+        this.chain = [
+            //GENESIS BLOCK (1ST BLOCK HARD CODED)
+            new Block(0,"asdadadadasd","asadasdadas",new Date().getTime(),new Transaction(100,"genesis", "satoshi"))
+        ];
+    }
+}
