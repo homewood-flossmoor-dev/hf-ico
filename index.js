ede3ff0a8c3a0786b593735beae936b7f5df3f5b
Juan Romero <75763058+jromerooo2@users.noreply.github.com>
2022-10-23 15:33:50 -0500

    initialize all of the users with points and handling when no more points

--- index.js
+++ index.js
@@ -1,118 +1,32 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-const crypto = __importStar(require("crypto"));
-const Block_1 = __importDefault(require("./lib/Block"));
-const Transaction_1 = __importDefault(require("./lib/Transaction"));
 const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
-app.get('/', (req, res) => {
-    res.send("<body>" +
-        "<h1>Hello World</h1>" +
-        "</body>"
-        + "<script src='/socket.io/socket.io.js'></script><script>var socket = io();</script>");
+const _ = require('lodash');
+const connected = [];
+app.use(express.static(__dirname));
+app.get('/play', (req, res) => {
+    res.sendFile(__dirname + '/client/play.html');
 });
+//waiting for connections on the play route
+let insert = {};
 io.on('connection', (socket) => {
-    console.log('a user connected');
+    insert = { id: socket.id, name: 'player', coin: 200 };
+    connected.push(insert);
+    io.emit("info", insert);
+    socket.on('disconnect', () => {
+        let b = _.findIndex(connected, function (el) { return el.id == socket.id; });
+        connected.splice(b, 1);
+    });
+    console.log(connected);
+});
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/client/index.html');
 });
 server.listen(3000, () => {
     console.log('listening on *:3000');
 });
-class Wallet {
-    constructor() {
-        this.coin = 0;
-        const keypair = crypto.generateKeyPairSync('rsa', {
-            modulusLength: 2048,
-            publicKeyEncoding: { type: 'spki', format: 'pem' },
-            privateKeyEncoding: { type: 'pkcs8', format: 'pem' },
-        });
-        this.privateKey = keypair.privateKey;
-        this.publicKey = keypair.publicKey;
-    }
-    sendMoney(amount, payeePublicKey) {
-        const transaction = new Transaction_1.default(amount, this.publicKey, payeePublicKey);
-        const sign = crypto.createSign('SHA256');
-        sign.update(transaction.toString()).end();
-        const signature = sign.sign(this.privateKey);
-        Chain.instance.addBlock(transaction, this.publicKey, signature, payeePublicKey);
-    }
-    readProps() {
-        return this.coin;
-    }
-}
-// The blockchain
-class Chain {
-    constructor() {
-        this.accounts = [];
-        this.chain = [
-            // Genesis block
-            new Block_1.default(0, '', new Transaction_1.default(100, 'genesis', 'satoshi'))
-        ];
-    }
-    // Most recent block
-    get lastBlock() {
-        return this.chain[this.chain.length - 1];
-    }
-    // Proof of work system
-    mine(nonce) {
-        let solution = 1;
-        console.log('⛏️  mining...');
-        while (true) {
-            const hash = crypto.createHash('MD5');
-            hash.update((nonce + solution).toString()).end();
-            const attempt = hash.digest('hex');
-            if (attempt.substr(0, 4) === '0000') {
-                console.log(`Solved: ${solution}`);
-                return solution;
-            }
-            solution += 1;
-        }
-    }
-    // Add a new block to the chain if valid signature & proof of work is complete
-    addBlock(transaction, senderPublicKey, signature, payeePublicKey) {
-        const verify = crypto.createVerify('SHA256');
-        verify.update(transaction.toString());
-        const isValid = verify.verify(senderPublicKey, signature);
-        if (isValid) {
-            const newBlock = new Block_1.default(this.chain.length + 1, this.lastBlock.hash, transaction);
-            this.mine(newBlock.nonce);
-            this.chain.push(newBlock);
-            this.accounts.push({ publicKey: senderPublicKey, privateKey: '', coin: transaction.amount });
-        }
-    }
-}
-// Singleton instance
-Chain.instance = new Chain();
-// Example usage
-const satoshi = new Wallet();
-const bob = new Wallet();
-const alice = new Wallet();
--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import { Socket } from 'socket.io';
+import { createBrotliCompress } from 'zlib';
 
 
 const express = require('express');
@@ -7,16 +8,47 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
+const _ = require('lodash');
 
-app.get('/', (req: any, res:any) => {
-    res.send("");
+interface User {
+  id: string;
+  name: string;
+  coin: number;
+}
+const connected : User[] = []; 
+
+app.use(express.static(__dirname));
+
+
+app.get('/play', (req: any, res:any) => {
+  res.sendFile(__dirname + '/client/play.html');
 });
-  
+
+  //waiting for connections on the play route
+  let insert: User = {} as User;
+
   io.on('connection', (socket: Socket) => {
-    console.log('a user connected');
+    
+    insert = {id: socket.id, name: 'player', coin: 200};
+
+    connected.push(insert);
+
+    io.emit("info", insert);
+
+
+    socket.on('disconnect', ()=>{
+      let b = _.findIndex(connected, function(el:User) { return el.id == socket.id; });
+      connected.splice(b, 1);
+    })
+    console.log(connected);
   });
   
-  server.listen(3000, () => {
+  
+app.get('/', (req: any, res:any) => {
+  res.sendFile(__dirname + '/client/index.html');
+});
+
+server.listen(3000, () => {
     console.log('listening on *:3000');
-  });
+});
   
